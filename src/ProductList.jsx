f8cd5182764ac4c590d691c18483191f4f5aef34
Pisek-K <pisekkidsawanon@gmail.com>
2024-09-05 08:28:43 +0700

    homework with challenge

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,48 +4,79 @@ export default function ProductList(props) {
   const [product, setProduct] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [skip, setSkip] = useState(0);
+  const [total, setTotal] = useState(0);
   const { searchFilter } = props;
-
+  const Limit = 20;
   const fetchData = async () => {
     try {
-    const resp = await fetch(
-      "https://dummyjson.com/products?limit=10&skip=10&select=title,price"
-    );
-    const data = await resp.json();
-    const result = data.products.filter(
-      (el) =>
-        String(el.title).toLowerCase().includes(searchFilter.toLowerCase()) ||
-        String(el.price).toLowerCase().includes(searchFilter.toLowerCase())
-    );
-    setProduct(result);
-  } catch(err){
-    setError(err)
-  }finally{
-    setLoading(false)
-  }}
+      const resp = await fetch(
+        `https://dummyjson.com/products/search?q=${searchFilter}&limit=${Limit}&skip=${skip}`
+      );
+      const data = await resp.json();
+      // const result = data.products.filter(
+      //   (el) =>
+      //     String(el.title).toLowerCase().includes(searchFilter.toLowerCase()) ||
+      //     String(el.price).toLowerCase().includes(searchFilter.toLowerCase())
+      // );
+      setTotal(data.total);
+      const result = data.products;
+      setProduct(result);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     setTimeout(() => {
       fetchData();
-    }, 2000);
+    }, 1000);
     return () => {
       clearTimeout();
     };
   }, [searchFilter]);
 
+  useEffect(() => {
+    fetchData();
+  }, [skip]);
+
+  const handleClick = (num) => {
+    setSkip((prev) => prev + num);
+  };
+
   if (loading) {
     return <div>Loading...</div>;
-    }
-      if (error) {
+  }
+  if (error) {
     return <div>Error: {error.message}</div>;
-    }
-    
+  }
+
   return (
+    // <div>
+    //   {true &&
+    //     product.map((el, index) => (
+    //       <p key={index}>{` ${el.title} |  ${el.price}`}</p>
+    //     ))}
+    // </div>
     <div>
-      {true &&
-        product.map((el, index) => (
-          <p key={index}>{` ${el.title} |  ${el.price}`}</p>
+      {skip + 20 < total && (
+        <button onClick={() => handleClick(20)}>next</button>
+      )}
+      <p>
+        {skip + 1}-{skip + 20 < total ? skip + 20 : total} of {total}
+      </p>
+      {skip + 1 > 1 && (
+        <button onClick={() => handleClick(-20)}>previous</button>
+      )}
+      <ul>
+        {product.map((el) => (
+          <li>
+            {el.title} | {el.price}
+          </li>
         ))}
+      </ul>
     </div>
   );
 }
